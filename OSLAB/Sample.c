4b57a30b2c345ab01f37f5e54855f686e112f8fe
Harshith <94514612+HarshithDee@users.noreply.github.com>
2022-08-30 11:17:36 +0530

    D Harshith completed sample.c with C Source Code

--- OSLAB/Sample.c
+++ OSLAB/Sample.c
@@ -1,5 +1,13 @@
-Name Of the Candidate: D. Harshith
-Roll No: 204G1A0538
-Title of the Experiment: CPU Scheduling Using FCFS Algorithm
-Date of Creation: 30-08-2022
-Date of Execution: 30-08-2022
+/*  Name Of the Candidate: D. Harshith
+    Roll No: 204G1A0538
+    Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+    Date of Creation: 30-08-2022
+    Date of Execution: 30-08-2022
+*/
+
+// Source Code
+#include <stdio.h>
+int main(){
+	printf("Hello World\n");
+ }
+  
